65defe08bccc8a11085d71a4603087b7884c2830
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2021-12-29 22:01:37 -0500

    init

---
+++ tasks/init.sh
@@ -0,0 +1,4 @@
+echo "Created '.apirc' file"
+curl -o ./.apirc https://gist.githubusercontent.com/matiasvlevi/65b89f4d9c777b4f1e4e4ec87f49f876/raw/cfde743cc3aadc8a989535745d6dafc70ec5249e/.apirc
+echo "Created '.env' file"
+curl -o ./.env https://gist.githubusercontent.com/matiasvlevi/179936cd9818c257974b5abd108b0be8/raw/8c478f2e55693c7777cb24fdc77baf524d81a4de/pilog.env
--- tasks/setup.sh
+++ tasks/setup.sh
@@ -1,3 +1,2 @@
-mkdir bin
 npm ci
 npm run build
