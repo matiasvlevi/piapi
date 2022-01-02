72d7906a60abaad2065b6574a27b568b5426e631
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2022-01-02 18:47:57 -0500

    Update init.sh

--- tasks/init.sh
+++ tasks/init.sh
@@ -1,6 +1,6 @@
-curl -o ./.apirc https://gist.githubusercontent.com/matiasvlevi/821d40a221e404208b9f2e713f230598/raw/4b2f3ae469388d15967427fe32d016a9492d6afc/.apirc
+curl -o ./.apirc https://gist.githubusercontent.com/matiasvlevi/821d40a221e404208b9f2e713f230598/raw/6044dec5bcec8d2302693bf1e7990b127b7ae2c6/.apirc
 echo "Created '.apirc' file"
-curl -o ./.env https://gist.githubusercontent.com/matiasvlevi/179936cd9818c257974b5abd108b0be8/raw/8c478f2e55693c7777cb24fdc77baf524d81a4de/pilog.env
+curl -o ./.env https://gist.githubusercontent.com/matiasvlevi/179936cd9818c257974b5abd108b0be8/raw/bb7188856a499a0b8f5e934300a27497c0271596/pilog.env
 echo "Created '.env' file"
 
 echo "Installing dependencies: "
