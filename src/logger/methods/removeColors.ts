bf01e5dbf6de1b5fbdfbb13de8cd208899a85131
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2022-01-05 14:41:51 -0500

    Update removeColors.ts

--- src/logger/methods/removeColors.ts
+++ src/logger/methods/removeColors.ts
@@ -1,8 +1,8 @@
 export function removeColors(msg: string) {
-  let matches: any[] = [...msg.matchAll(/\x1b\[[0-9].?m/gm)];
+  let matches: string[] = msg.match(/\x1b\[[0-9].?m/gm) || [];
   let colorLess: string = msg;
   for (let i = 0; i < matches.length; i++) {
-    colorLess = colorLess.replace(matches[i][0], '');
+    colorLess = colorLess.replace(matches[i], '');
   }
   return colorLess;
 }
